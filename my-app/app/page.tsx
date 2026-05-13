c8934064959256309d229b5414d53bd5ad29e254
anurag8506 <anuragsinghyadav191@gmail.com>
2026-05-13 19:02:21 +0530

    ad_:

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -32,7 +32,7 @@ export default function ProfileFormPage() {
     setMessage("");
 
     try {
-      const response = await fetch("https://constructioncourse.co.uk/api/profile", {
+      const response = await fetch("http://localhost:8000/api/profile", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.get("/api/profile", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
